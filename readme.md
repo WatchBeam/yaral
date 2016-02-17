b3b547deb7c2a97d3a8b48e9381b1f20e20ed4ef
Connor Peet <connor@peet.io>
2016-02-16 19:22:32 -0800

    Init

---
+++ readme.md
@@ -0,0 +1,34 @@
+# Routebox
+
+[![Build Status](https://img.shields.io/travis/WatchBeam/yarl.svg?style=flat-square)](https://travis-ci.org/WatchBeam/yarl)
+
+Yarl is Yet Another Rate Limit plugin for Hapi. But, unlike others, it does several nice things!
+ - Integrates with you server's Catbox cache
+ - Allows you to limit with custom attributes, not just the user's IP.
+ - Allows you to limit certain responses, add limiting globally, and adjust the limiting endpoint-by-endpoint
+
+### Concepts
+
+ * The entire server can be limited under one rule, and additionally routes can provide their own limiting rules that are _appended_ to the global rule.
+ * Each rule has a list of status codes that it can limit. This allows you to, for example, limit invalid response codes at a lower rate than successful response codes. Responses "bubble up" to the first rule that can handle them. If no rules handle that code, it will not be limited.
+ * Limit rules are specified using a maximum number of requests per unit time, similar to the way the Twitter API works.
+
+### Configuration
+
+The following options are available when you register Yarl:
+ - `buckets` is an array of interval config for [Limitus](https://github.com/MCProHosting/limitus#limitusrulename-rule) intervals. Each item should have:
+    - An identifying `name`
+    - An `interval` that allows a `max` number of requests.
+    - An `id` function that takes a Hapi request object and returns a string, number or object that identifies the requester.
+    - A list of `codes` that specify response codes that count towards this bucket's limit. Responses not in this range will not be limited. Defaults to `['2xx', '3xx']`. *Tip:* to limit all responses, use `['xxx']`.
+ - `default` is a bucket `name` or array of names of the bucket applied to all routes. Defaults to `[]`. Buckets are matched first to last.
+ - `cache` is the cache name (as configured in the Hapi server) used to store rate limiting data. Defaults to the server's default cache.
+ - `enabled` is a boolean whether to enable rate limiting. Useful to disable limiting in tests and development. Default to `true`.
+ - `includeHeaders` specifies whether rate limit headers should be included in the response.
+ - `limitus` is a Limitus instance to use for this rate limiting. Defaults to `new Limitus()`.
+
+You can also configure options on a per-route basis in `config.plugins.yarl`:
+ - `buckets` specifies the bucket `name` or array of of the rate limit buckets to use in addition to the configured `default` rules. Buckets are matched first to last.
+ - `enabled` is a boolean which allows you to override a true `enabled` global configuration. This can be used to exclude routes from global rate limits. Defaults to `true`.
+
+Alternately, for routes, you can specify a single string or array as a shorthand for buckets.
---
+++ test/yarl.test.js
@@ -0,0 +1,257 @@
+const Hapi = require('hapi');
+const Boom = require('boom');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const async = require('async');
+
+describe('routebox', function () {
+    var server;
+    var clock;
+    beforeEach(function (done) {
+        clock = sinon.useFakeTimers();
+        server = new Hapi.Server();
+        server.connection();
+        server.start(done);
+    });
+
+    afterEach(function (done) {
+        server.stop(done);
+        clock.restore();
+    });
+
+    const id = (req) => req.query.l;
+
+    const testSequence = (data, done) => {
+        async.series(data.map((d) => {
+            return (callback) => {
+                server.inject({ method: 'GET', url: d.url }, (res) => {
+                    expect(res.statusCode).to.equal(d.status);
+                    expect(res.headers['x-ratelimit-remaining']).to.equal(d.left);
+
+                    if (d.then) d.then();
+
+                    callback();
+                });
+            };
+        }), done);
+    };
+
+    describe('limiting functionality', function () {
+        beforeEach(function (done) {
+            server.register({
+                register: require('../'),
+                options: {
+                        buckets: [
+                        { id, name: 'a', interval: 1000, max: 2 },
+                        { id, name: 'b', interval: 1500, max: 5, codes: ['4xx'] },
+                        { id, name: 'c', interval: 1000, max: 2, codes: ['xxx'] },
+                    ]
+                }
+            }, done);
+        });
+
+        const testBasicLimit = (done) => {
+            testSequence([
+                { url: '/?l=a', status: 200, left: 1 },
+                { url: '/?l=a', status: 200, left: 0 },
+                { url: '/?l=a', status: 429, left: 0 },
+                { url: '/?l=b', status: 200, left: 1, then: () => clock.tick(1001) },
+                { url: '/?l=a', status: 200, left: 1 },
+            ], done);
+        };
+
+        it('limits basic stuff using single string shorthand', function (done) {
+            server.route({
+                method: 'get', path: '/',
+                config: {
+                    plugins: { yarl: 'a' },
+                    handler: (req, reply) => reply('ok'),
+                },
+            });
+
+            testBasicLimit(done);
+        });
+
+        it('limits basic stuff using array shorthand', function (done) {
+            server.route({
+                method: 'get', path: '/',
+                config: {
+                    plugins: { yarl: ['a'] },
+                    handler: (req, reply) => reply('ok'),
+                },
+            });
+
+            testBasicLimit(done);
+        });
+
+        it('limits basic stuff using full config', function (done) {
+            server.route({
+                method: 'get', path: '/',
+                config: {
+                    plugins: { yarl: { buckets: ['a'] }},
+                    handler: (req, reply) => reply('ok'),
+                },
+            });
+
+            testBasicLimit(done);
+        });
+
+        it('uses multiple bucket correctly', function (done) {
+            var res = () => Boom.badRequest('400');
+            server.route({
+                method: 'get', path: '/',
+                config: {
+                    plugins: { yarl: ['b', 'a'] },
+                    handler: (req, reply) => reply(res()),
+                },
+            });
+
+            testSequence([
+                { url: '/?l=a', status: 400, left: 4 },
+                { url: '/?l=a', status: 400, left: 3, then: () => { res = () => 'ok'; }},
+                { url: '/?l=a', status: 200, left: 1 },
+                { url: '/?l=a', status: 200, left: 0 },
+                { url: '/?l=a', status: 429, left: 0, then: () => {
+                    clock.tick(1100);
+                    res = () => Boom.badRequest('400');
+                } },
+                { url: '/?l=a', status: 400, left: 2 },
+                { url: '/?l=a', status: 400, left: 1 },
+                { url: '/?l=a', status: 400, left: 0 },
+                { url: '/?l=a', status: 429, left: 0, then: () => { res = () => 'ok'; }},
+                { url: '/?l=a', status: 429, left: 0 },
+            ], done);
+        });
+
+        it('does not limit non-matching', function (done) {
+            var res = () => Boom.badRequest('400');
+            server.route({
+                method: 'get', path: '/',
+                config: {
+                    plugins: { yarl: ['a'] },
+                    handler: (req, reply) => reply(res()),
+                },
+            });
+
+            server.route({
+                method: 'get', path: '/asdf',
+                config: {
+                    handler: (req, reply) => reply('ok'),
+                },
+            });
+
+            testSequence([
+                { url: '/asdf', status: 200, left: undefined },
+                { url: '/?l=a', status: 400, left: undefined, then: () => { res = () => 'ok'; }},
+                { url: '/?l=a', status: 200, left: 1 },
+            ], done);
+        });
+    });
+
+    describe('global handlers', function () {
+        beforeEach(function (done) {
+            server.register({
+                register: require('../'),
+                options: {
+                    buckets: [
+                        { id, name: 'a', interval: 1000, max: 2 },
+                        { id, name: 'b', interval: 1500, max: 5, codes: ['4xx'] },
+                        { id, name: 'c', interval: 1000, max: 2, codes: ['xxx'] },
+                    ],
+                    default: ['a']
+                }
+            }, done);
+        })
+
+        it('limits correctly', function (done) {
+            var res = () => Boom.badRequest('400');
+            server.route({
+                method: 'get', path: '/',
+                config: {
+                    plugins: { yarl: ['b'] },
+                    handler: (req, reply) => reply(res()),
+                },
+            });
+
+            testSequence([
+                { url: '/?l=a', status: 400, left: 4 },
+                { url: '/?l=a', status: 400, left: 3, then: () => { res = () => 'ok'; }},
+                { url: '/?l=a', status: 200, left: 1 },
+                { url: '/?l=a', status: 200, left: 0 },
+                { url: '/?l=a', status: 429, left: 0, then: () => {
+                    clock.tick(1100);
+                    res = () => Boom.badRequest('400');
+                } },
+                { url: '/?l=a', status: 400, left: 2 },
+                { url: '/?l=a', status: 400, left: 1 },
+                { url: '/?l=a', status: 400, left: 0 },
+                { url: '/?l=a', status: 429, left: 0, then: () => { res = () => 'ok'; }},
+                { url: '/?l=a', status: 429, left: 0 },
+            ], done);
+        });
+
+        it('respects by-route disable', function (done) {
+            server.route({
+                method: 'get', path: '/asdf',
+                config: {
+                    plugins: { yarl: { enabled: false }},
+                    handler: (req, reply) => reply('ok'),
+                },
+            });
+
+            testSequence([
+                { url: '/asdf', status: 200, left: undefined },
+            ], done);
+        })
+    });
+
+    it('respects disabled', function (done) {
+        server.register({
+            register: require('../'),
+            options: {
+                buckets: [
+                    { id, name: 'a', interval: 1000, max: 2 },
+                ],
+                enabled: false,
+            }
+        }, () => {
+            server.route({
+                method: 'get', path: '/',
+                config: {
+                    plugins: { yarl: ['a'] },
+                    handler: (req, reply) => reply('ok'),
+                },
+            });
+
+            testSequence([
+                { url: '/', status: 200, left: undefined },
+            ], done);
+        });
+    });
+
+    it('omits headers when requested', function (done) {
+        server.register({
+            register: require('../'),
+            options: {
+                buckets: [
+                    { id, name: 'a', interval: 1000, max: 2 },
+                ],
+                includeHeaders: false,
+            }
+        }, () => {
+            server.route({
+                method: 'get', path: '/',
+                config: {
+                    plugins: { yarl: ['a'] },
+                    handler: (req, reply) => reply('ok'),
+                },
+            });
+
+            testSequence([
+                { url: '/?l=a', status: 200, left: undefined },
+                { url: '/?l=a', status: 200, left: undefined },
+                { url: '/?l=a', status: 429, left: undefined },
+            ], done);
+        });
+    });
+});
